3bdd42ef3a5781f2fdfec5b7c3f846789e4b9e7c
Kelly Alzate <kejoh07@gmail.com>
2022-03-09 00:52:06 +0100

    Add Icons,
    Co-authored-by: Oscar <oscaravg27@gmail.com>
    Co-authored-by: Vanina <vaniwinnik@gmail.com>
    Co-authored-by: Alberto <jgonzalez48@ucol.mx>

--- Reservacion/src/screens/BookingScreen.js
+++ Reservacion/src/screens/BookingScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { ButtonNext } from '../components/Button/index';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const BookingScreen = () => {
   return (
     <View style={styles.container}>
         <View style={styles.containerButton}> 
+        <Icon 
+        name="plane" 
+        size={50}/>
 
             <ButtonNext />
         </View>
