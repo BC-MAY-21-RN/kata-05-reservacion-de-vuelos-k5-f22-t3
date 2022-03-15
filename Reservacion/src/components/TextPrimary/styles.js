c9625db9ddf078838423838a081437c0a42f08ec
Kelly Alzate <kejoh07@gmail.com>
2022-03-16 00:44:56 +0100

    Remove unecessary code
    Co-authored-by: Vanina <vaniwinnik@gmail.com>
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Oscar <oscaravg27@gmail.com>
    Co-authored-by: Alberto <jgonzalez48@ucol.mx>

--- Reservacion/src/components/TextPrimary/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    text:{
-        fontWeight: 'bold',
-        marginVertical: 4,
-        marginHorizontal: 0
-    }
-})
