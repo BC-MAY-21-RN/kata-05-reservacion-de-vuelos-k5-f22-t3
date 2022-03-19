f19629a0c079d95ccfe52fbd449ae00715813a13
Kelly Alzate <kejoh07@gmail.com>
2022-03-19 01:27:41 +0100

    Refactoring Sing up,
    Co-authored-by: Alberto Gonzalez <w01fgxng@users.noreply.github.com>
    Co-authored-by: Vanina Ayelen Winnik <VaninaWinnik@users.noreply.github.com>
    Co-authored-by: Omar Cardona <omar-cardona@users.noreply.github.com>
    Co-authored-by: Demis Alberto Rincón Martínez <darmfma@gmail.com>
    Co-authored-by: Oscar Villarraga  <osvilla27@users.noreply.github.com>

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignupScreen } from '../../src/screens/SignupScreen/index'
-import { LoginScreen } from '../../src/screens/LoginScreen/index';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +8,6 @@ export const Navigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Signup" component={SignupScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
--- src/screens/SignupScreen/index.js
+++ src/screens/SignupScreen/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { ButtonPrimary } from '../../components/ButtonPrimary/index';
 import { ButtonSecond } from '../../components/ButtonSecond/index';
@@ -19,19 +19,22 @@ const {
   canSubmitSingUp
 } = useAuthForm()
 
+const [isLoginScreen, setIsLoginScreen] = useState(false)
 
   const signInputs = [{
     ...name,
     label:'First Name',
     placeholder:"username",
     style:!!name?styles.inputSelected:styles.input,
+    hide:isLoginScreen
   },
   {
     ...email,
     label:'Email',
     placeholder:"email",
     style:!!email?styles.inputSelected:styles.input,
-    isRequiered:true
+    isRequiered:true,
+    
   },
   {
     ...password,
@@ -39,7 +42,8 @@ const {
     placeholder:"password",
     style:!!password?styles.inputSelected:styles.input,
     secureTextEntry:true,
-    isRequiered:true
+    isRequiered:true,
+   
   }]
 
 
@@ -48,23 +52,28 @@ const {
   }
   return ( 
     <View style={styles.container}> 
-        {signInputs.map((item)=><InputText key={item.label} {...item}/> )}
+        {signInputs.map((item)=>!item.hide&&<InputText key={item.label} {...item}/> )}
+       {
+        !isLoginScreen&&
+       <>
         <Text>Use 8 or more characters with a mix of letters, numbers and symbols</Text>        
         <Check value={terms.value}  onValueChange={terms.onChangeText} text={"I agree to the Terms and Privacy Policy"}/>
         <Check value={subscribe.value}  onValueChange={subscribe.onChangeText}  text={"Suscribe for select product updates."}/>
+       </>
+       } 
         <View style={styles.content}>
           <ButtonPrimary 
             onPress={handleSingUp}
             disabled={canSubmitSingUp}
-            text={"Sign Up"}/>
+            text={isLoginScreen?"Login":"Sign Up"}/>
           <ButtonSecond 
             onPress={() => navigation.navigate('Login')} 
-            text={"Sing Up whith Google"}
+            text={`${isLoginScreen?'Login ':"Sing Up "}with Google`}
           />
             <View style={styles.containerFooter}>
-                <Text style={styles.textSize}>Already have an account?</Text>
-                <TouchableOpacity  onPress={() => navigation.navigate('Login')}>
-                    <Text style={{...styles.textSize,color: '#5b6ef7'}}> Login</Text>
+                <Text style={styles.textSize}>{isLoginScreen?`Don't you have an account?`:'Already have an account?'}</Text>
+                <TouchableOpacity  onPress={() => setIsLoginScreen(!isLoginScreen)}>
+                    <Text style={{...styles.textSize,color: '#5b6ef7'}}> {isLoginScreen?'Signup': 'Login'}</Text>
                 </TouchableOpacity>
             </View>
         </View>
