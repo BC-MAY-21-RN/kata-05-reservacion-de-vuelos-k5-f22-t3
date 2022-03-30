1607d6d8528318399cebb0f63a0fead17e44da71
osvilla27 <oscaravg27@gmail.com>
2022-03-29 19:34:57 -0500

    Finished Number Picker

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignupScreen } from '../../src/screens/SignupScreen/index'
-import { PickerScreen } from '../screens/PickerScreen';
 
 const Stack = createNativeStackNavigator();
 
 export const Navigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Picker" component={PickerScreen} /> 
       <Stack.Screen name="Signup" component={SignupScreen} />
     </Stack.Navigator>
   );
