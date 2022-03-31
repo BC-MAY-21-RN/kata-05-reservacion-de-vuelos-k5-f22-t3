400c0e363154a27b992e68d16e6c9387eff53cdf
Alberto Gonzalez <73139470+w01fgxng@users.noreply.github.com>
2022-03-30 18:44:10 -0600

    Implement CalendarComponent.

---
+++ src/components/FlightsCalendar/index.js
@@ -0,0 +1,49 @@
+import { Calendar, LocaleConfig, CalendarProps } from "react-native-calendars";
+import React, { useEffect , useState, useCallback, useMemo,Fragment} from "react";
+import {StyleSheet, View, ScrollView, Text, TouchableOpacity, } from 'react-native';
+
+export const FlightsCalendar = () => {
+
+
+
+    const INITIAL_DATE = '2020-02-02';
+    const [selected, setSelected] = useState(INITIAL_DATE);
+
+    const onDayPress = useCallback(day => {
+        setSelected(day.dateString);
+    }, []);
+    const marked = useMemo(() => {
+        return {
+            [selected]: {
+                selected: true,
+                disableTouchEvent: true,
+                selectedColor: '#5b6ef7',
+                selectedTextColor: 'white'
+            }
+        };
+    }, [selected]);
+    return (
+        <Fragment>
+            <Text style={styles.text}>Calendar with selectable date</Text>
+            <Calendar
+
+                enableSwipeMonths
+                current={INITIAL_DATE}
+                style={styles.calendar}
+                onDayPress={onDayPress}
+                markedDates={marked}
+            />
+        </Fragment>)
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+    calendar: {
+        marginBottom: 10
+        
+
+    },
+})
