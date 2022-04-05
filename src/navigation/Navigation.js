13831b42a2c096808e5ca2d165f2631e65ff7491
Kelly Alzate <kejoh07@gmail.com>
2022-04-05 02:01:42 +0200

    Add the screens origin, destiny, calendar and passengers,
    Co-authored-by: Vanina Ayelen Winnik <VaninaWinnik@users.noreply.github.com>
    Co-authored-by: Alberto Gonzalez <w01fgxng@users.noreply.github.com>
    Co-authored-by: Oscar <oscaravg27@gmail.com>

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -4,6 +4,10 @@ import { MyFlightsScreen } from '../screens/MyFlightsScreen/index';
 import { BookingScreen } from '../screens/BookingScreen/index';
 import { SignupScreen } from '../screens/SignupScreen/index';
 import { infoUser } from '../library/utils/auth';
+import { CalendarScreen } from '../screens/CalendarScreen/index';
+import { OriginScreen } from '../screens/OriginScreen/index';
+import { DestinyScreen } from '../screens/DestinyScreen/index';
+import { PassengersScreen } from '../screens/PassengersScreen/index';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +17,12 @@ export const Navigation = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       {!currentUser?<Stack.Screen name="Signup" component={SignupScreen} />:
       <>
+      <Stack.Screen name="Origin" component={OriginScreen} />
+      <Stack.Screen name="Destiny" component={DestinyScreen} />
+      <Stack.Screen name="CalendarFlights" component={CalendarScreen} />
+      <Stack.Screen name="Passengers" component={PassengersScreen} />
       <Stack.Screen name="Myflights" component={MyFlightsScreen} />
-      <Stack.Screen name="Booking" component={BookingScreen} />
+      {/*<Stack.Screen name="Booking" component={BookingScreen} />*/}
       </>}
     </Stack.Navigator>
   );
